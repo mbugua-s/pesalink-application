4ae7f025618865b73fd825e9b880b32508268860
mbugua-s <smbuguaaa@gmail.com>
2025-04-20 18:57:06 +0300

    [feat] Added user details card page

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -5,19 +5,139 @@ import type { User } from '@/types/User'
 import { computed } from 'vue'
 import { useUserStore } from '@/stores/UserStore'
 import Card from 'primevue/card'
+import Divider from 'primevue/divider'
+import UserCardRow from '@/components/UserCardRow.vue'
 
 const userStore = useUserStore()
 const selectedUser = computed<User>(() => userStore.getSelectedUser)
 </script>
 
 <template>
-	<Card>
-		<template #title> User Details </template>
+	<Card class="card-container">
+		<template #title> <h1>User Details</h1> </template>
 		<template #content>
-			Sample card showing selected user
-			{{ selectedUser }}
+			<div class="user-card">
+				<div class="user-card-key-details">
+					<img src="/src/assets/person.jfif" alt="profile-photo" />
+					<p class="user-details-name">{{ selectedUser.name }}</p>
+					<p class="user-details-user-name">"{{ selectedUser.username }}"</p>
+				</div>
+
+				<Divider layout="vertical" />
+				<div class="user-card-additional-details">
+					<div class="details-section">
+						<p class="details-label">General</p>
+						<div class="details-group">
+							<UserCardRow
+								data-key="Email"
+								:data-value="selectedUser.email"
+								:remove-border-top="true"
+							/>
+							<UserCardRow data-key="Phone Number" :data-value="selectedUser.phone" />
+							<UserCardRow data-key="Website" :data-value="selectedUser.website" />
+						</div>
+					</div>
+					<Divider />
+					<div class="details-section">
+						<p class="details-label">Address</p>
+						<div class="details-group">
+							<UserCardRow
+								data-key="Street"
+								:data-value="selectedUser.address.street"
+								:remove-border-top="true"
+							/>
+							<UserCardRow
+								data-key="Suite"
+								:data-value="selectedUser.address.suite"
+							/>
+							<UserCardRow data-key="City" :data-value="selectedUser.address.city" />
+							<UserCardRow
+								data-key="Zipcode"
+								:data-value="selectedUser.address.zipcode"
+							/>
+							<UserCardRow
+								data-key="Geographic Location"
+								:data-value="`${selectedUser.address.geo.lat}, ${selectedUser.address.geo.lng}`"
+							/>
+						</div>
+					</div>
+					<Divider />
+					<div v-if="selectedUser.company" class="details-section">
+						<p class="details-label">Company</p>
+						<div class="details-group">
+							<UserCardRow
+								data-key="Name"
+								:data-value="selectedUser.company?.name"
+								:remove-border-top="true"
+							/>
+							<UserCardRow
+								data-key="Catchphrase"
+								:data-value="selectedUser.company?.catchPhrase"
+							/>
+							<UserCardRow data-key="Bs" :data-value="selectedUser.company?.bs" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</template>
 	</Card>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card-container {
+	max-width: 70vw;
+	margin: auto;
+}
+
+.user-card {
+	display: flex;
+	flex: 1;
+}
+
+.user-card-key-details {
+	display: flex;
+	flex-direction: column;
+	/* width: 30%; */
+	align-items: center;
+}
+
+.user-card-additional-details {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+}
+
+img {
+	border-radius: 50%;
+	width: 15vw;
+}
+
+.details-section {
+	display: flex;
+	flex: 1;
+	justify-content: space-between;
+	width: 100%;
+	padding-left: 2vw;
+	padding-right: 2vw;
+}
+
+.details-label {
+	font-size: 30px;
+}
+
+.details-group {
+	width: 35vw;
+}
+
+.user-details-name {
+	/* margin: auto; */
+	font-size: 25px;
+	font-weight: bold;
+}
+
+.user-details-user-name {
+	/* margin: auto; */
+	font-size: 20px;
+	font-style: italic;
+}
+</style>
---
+++ src/components/UserCardRow.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Props {
+	dataKey: string
+	dataValue: string
+	removeBorderTop?: boolean
+}
+
+const { dataKey, dataValue, removeBorderTop = false } = defineProps<Props>()
+</script>
+
+<template>
+	<div class="row-container" :style="removeBorderTop && 'border-top: none'">
+		<p class="row-key">{{ dataKey }}</p>
+		<p>{{ dataValue }}</p>
+	</div>
+</template>
+
+<style scoped>
+.row-container {
+	display: flex;
+	flex: 1;
+	justify-content: space-between;
+	/* max-width: 35vw; */
+	border-top: 1px solid var(--p-divider-border-color);
+}
+
+.row-key {
+	font-weight: bold;
+	font-style: italic;
+}
+</style>
